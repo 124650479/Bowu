47082df6d46303d13bb3f1c8d1bffd1892e8dd44
lpz <1274650479@qq,com>
2022-06-06 20:30:00 +0800

    add pro and test api

--- app/src/main/java/fu/prm391/sampl/project/remote/ApiClient.java
+++ app/src/main/java/fu/prm391/sampl/project/remote/ApiClient.java
@@ -12,6 +12,8 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ApiClient {
 
+    //public static final String BASE_URL = "http://192.168.0.113:8089/";
+
     public static final String BASE_URL = "http://121.40.157.66:8089/";
 
     private static Retrofit getRetrofit() {
